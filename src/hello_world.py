274eb15a6058d12316a0e5a563fa536a2fcc0556
Danilo Galvão <danilommgalvao@gmail.com>
2019-02-21 09:38:54 -0300

    Incrementando o hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -2,3 +2,5 @@
 
 print('Olá Mundo!!!')
 print('Este código está no meu primeiro repositório GIT')
+print('Se quiser conferir mais é só acessar:')
+printf('\nhttps://github.com/Fisiquelaz/Git-minicurso/tree/master/src\n')
